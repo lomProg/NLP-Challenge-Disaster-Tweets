00015012b6274679e5ade22eb7156781494261d6
lo-mora <lorenzomora@thenewsroom.ai>
2023-05-05 11:17:13 +0200

    New func clean_text

--- prepocessing.py
+++ prepocessing.py
@@ -4,6 +4,7 @@ import gensim
 from typing import List
 from nltk.stem import WordNetLemmatizer
 from gensim.parsing.porter import PorterStemmer
+import emot
 
 
 from text_utils import PUNCTUATIONS, STOPWORDS, EMOTICONS, POS_EMOT, NEG_EMOT, POS_EMOJI, NEG_EMOJI
@@ -28,13 +29,55 @@ def extract_hashtag(text:str)->List[str]:
         hashtag = [w.lower() for w in hashtag] #lower case
     return hashtag
 
-def convert_emoticons(text):
+def convert_emoticons_old(text):
+    # TODO Evaluate function, latest version immediately below.
     for e in EMOTICONS:
         if EMOTICONS[e] in POS_EMOT:
             text = re.sub(u'(' + e + ')', 'positive', text)
         else:
             text = re.sub(u'(' + e + ')', 'negative', text)
     return text
+
+def convert_emoticons(text:str)->str:
+    """Converts all the emoticons present in the input text into the
+    corresponding token. Token value can be `:positive:`, `:neutral:` or
+    `:negative:`.
+
+    Parameters
+    ----------
+    text : str
+        Input text in which to apply the conversion.
+
+    Returns
+    -------
+    str
+        If emoticons were found in the text, the output will contain the
+        corresponding tokens instead of symbols. If instead nothing was found,
+        the output string matches the input one.
+    """
+    emot_obj = emot.core.emot()
+    emot_dict = emot_obj.emoticons(text)
+    
+    if not emot_dict['value']:
+        return text
+    
+    text_temp = text
+    for i in range(len(emot_dict['value'])):
+
+        idx = emot_dict['location'][i]
+        d = len(text_temp) - len(text)
+        text_temp = text_temp[0 : idx[0] + d:] + text_temp[idx[1] + d::]
+
+        if any(re.match(p, emot_dict['mean'][i], re.I) for p in POS_EMOT):
+            tkn = ':positive:'
+        elif any(re.match(n, emot_dict['mean'][i], re.I) for n in NEG_EMOT):
+            tkn = ':negative:'
+        else:
+            tkn = ':neutral:'
+
+        text_temp = text_temp[:idx[0] + d] + tkn + text_temp[idx[0] +d:]
+
+    return text_temp
 
 def convert_emoji(text):
     for e in POS_EMOJI:
@@ -65,6 +108,14 @@ def remove_neutral_emoji(text):
                                u"\u3030"
                                "]+", flags=re.UNICODE)
     return emoji_pattern.sub(r'', text)
+
+def remove_urls(text:str)->str:
+    """ The URLs are removed from the input text. """
+    return re.sub(r'http\S+', '', text)
+
+def lowercase(text):
+    """ Convert input text to lowercase. """
+    return text.lower()
 
 def remove_whitespaces(text):
     """ The blank spaces are removed from the input text. """
@@ -115,25 +166,11 @@ def remove_stopwords(text:str, s_w:set = STOPWORDS)->str:
     if isinstance(text, str):
         return ' '.join([word for word in text.split() if word not in s_w])
 
-def clean_text(text:str):
-    out_text = text
-
-def remove_digit(text:str)->str:
-    """Remove digits from text
-
-    Parameters
-    ----------
-    text : str
-        _description_
-
-    Returns
-    -------
-    str
-        _description_
-    """
+def remove_digits(text:str)->str:
+    """ Remove digits from text. """
     return ''.join([i for i in text if not i.isdigit()])
 
-def slang_conversion(text:str)->str:
+def convert_slang(text:str)->str:
     """Decontracts colloquial slang in extended form, e.g. 'U' is converted in 'You'
 
     Parameters
@@ -162,3 +199,23 @@ def stemming(text:str)->str:
     stemmer = PorterStemmer()
     return stemmer.stem_sentence(text)
 
+#########################
+def clean_text(text:str)->str:
+    # Remove URLs
+    out_text = remove_urls(text)
+    # Conversion emoticons
+    out_text = convert_emoticons(out_text)
+    # Conversion emoji
+    # Decontraction slangs
+    out_text = convert_slang(out_text)
+    # Text to lowercase
+    out_text = lowercase(out_text)
+    # Remove digits
+    out_text = remove_digits(out_text)
+    # Remove whitespaces
+    out_text = remove_whitespaces(out_text)
+    # Remove stopwords
+    out_text = remove_stopwords(out_text)
+    # Remove punctuation
+    out_text = remove_punctuation(out_text)
+    return out_text
