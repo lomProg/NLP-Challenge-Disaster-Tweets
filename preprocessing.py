da6090615344229222862d34ffbc0f185c8cd015
lorenzo-mora <l.mora4@campus.unimib.it>
2023-05-11 16:02:47 +0200

    Put funcs in order

--- preprocessing.py
+++ preprocessing.py
@@ -1,43 +1,15 @@
 import re
-import nltk
-import gensim
 from typing import List
 from nltk.stem import WordNetLemmatizer
 from gensim.parsing.porter import PorterStemmer
-import emot
 import time
 
 from text_utils import PUNCTUATIONS, STOPWORDS, EMOTICONS, POS_EMOT, NEG_EMOT
 from text_utils import SLANG, CONTRACTIONS, STMT_EMOJI, ADDITIONAL_EMOJI
 
-def extract_hashtag(text:str)->List[str]:
-    """It extracts all the hashtags from the text and it collects all of them in
-    a list.
-
-    Parameters
-    ----------
-    text : str
-        Text to preprocess.
-
-    Returns
-    -------
-    List[str]
-        List of hashtag without the '#'
-    """
-    hashtag = re.findall(r"#(\w+)", text)
-    if hashtag:
-        #check if list is not empty
-        hashtag = [w.lower() for w in hashtag] #lower case
-    return hashtag
-
-def convert_emoticons_old(text):
-    # TODO Evaluate function, latest version immediately below.
-    for e in EMOTICONS:
-        if EMOTICONS[e] in POS_EMOT:
-            text = re.sub(u'(' + e + ')', 'positive', text)
-        else:
-            text = re.sub(u'(' + e + ')', 'negative', text)
-    return text
+def remove_urls(text:str)->str:
+    """ The URLs are removed from the input text. """
+    return re.sub(r'http\S+', '', text)
 
 def convert_emoticons(text:str)->str:
     """Converts all the emoticons present in the input text into the
@@ -116,19 +88,65 @@ def remove_neutral_emoji(text):
                                "]+", flags=re.UNICODE)
     return emoji_pattern.sub(r'', text)
 
-def remove_urls(text:str)->str:
-    """ The URLs are removed from the input text. """
-    return re.sub(r'http\S+', '', text)
+def convert_slang(text:str)->str:
+    """Decontracts colloquial slang in extended form.
+    e.g. 'U' is converted in 'You'
+
+    Parameters
+    ----------
+    text : str
+        Input text in which to apply the conversion.
+
+    Returns
+    -------
+    str
+        _description_
+    """
+    new_text = []
+    for w in text.split():
+        if w.upper() in list(SLANG.keys()):
+            new_text.append(SLANG[w.upper()].lower())
+        else:
+            new_text.append(w)
+    return " ".join(new_text)
 
 def lowercase(text):
     """ Convert input text to lowercase. """
     return text.lower()
 
-def remove_whitespaces(text):
-    """ The blank spaces are removed from the input text. """
-    return " ".join(text.split())
+def decontract_text(text:str)->str:
+  """ Decontract english contracted forms to the extended ones.
+  e.g. I'm -> I am
+  """
+  for c in CONTRACTIONS.keys():
+    text = re.sub(c, CONTRACTIONS[c], text)
+  return text
 
-def remove_punctuation(text:str, extra_punc:str = None, wanted_punc:str = None)->str:
+def remove_digits(text:str)->str:
+    """ Remove digits from text. """
+    return ''.join([i for i in text if not i.isdigit()])
+
+def remove_stopwords(text:str, s_w:set = STOPWORDS)->str:
+    """It removes the entire set of stopwords s_w from the input text.
+
+    Parameters
+    ----------
+    text : str
+        Input text in which to apply the stopwords removal.
+    s_w : set, optional
+        Set of the stopwords to remove, by default STOPWORDS
+
+    Returns
+    -------
+    str
+        _description_
+    """
+    if isinstance(text, str):
+        return ' '.join([word for word in text.split() if word not in s_w])
+
+def remove_punctuation(text:str,
+                       extra_punc:str = None,
+                       wanted_punc:str = None)->str:
     """It removes punctuations marks from input text, adding an additional
     set of symbols if specified.
 
@@ -155,67 +173,11 @@ def remove_punctuation(text:str, extra_punc:str = None, wanted_punc:str = None)-
     trans = str.maketrans(dict.fromkeys(punc, ' '))
     return text.translate(trans)
 
-def remove_stopwords(text:str, s_w:set = STOPWORDS)->str:
-    """It removes the entire set of stopwords s_w from the input text.
+def remove_whitespaces(text):
+    """ The blank spaces are removed from the input text. """
+    return " ".join(text.split())
 
-    Parameters
-    ----------
-    text : str
-        Input text in which to apply the stopwords removal.
-    s_w : set, optional
-        Set of the stopwords to remove, by default STOPWORDS
-
-    Returns
-    -------
-    str
-        _description_
-    """
-    if isinstance(text, str):
-        return ' '.join([word for word in text.split() if word not in s_w])
-
-def remove_digits(text:str)->str:
-    """ Remove digits from text. """
-    return ''.join([i for i in text if not i.isdigit()])
-
-def convert_slang(text:str)->str:
-    """Decontracts colloquial slang in extended form, e.g. 'U' is converted in 'You'
-
-    Parameters
-    ----------
-    text : str
-        Input text in which to apply the conversion.
-
-    Returns
-    -------
-    str
-        _description_
-    """
-    new_text = []
-    for w in text.split():
-        if w.upper() in list(SLANG.keys()):
-            new_text.append(SLANG[w.upper()].lower())
-        else:
-            new_text.append(w)
-    return " ".join(new_text)
-
-def decontract_text(text:str)->str:
-  """ Decontract english contracted forms to the extended ones.
-  e.g. I'm -> I am
-  """
-  for c in CONTRACTIONS.keys():
-    text = re.sub(c, CONTRACTIONS[c], text)
-  return text
-
-def lemmatization(text:str)->str:
-    lemmatizer = WordNetLemmatizer()
-    return " ".join([lemmatizer.lemmatize(word) for word in text.split()])
-
-def stemming(text:str)->str:
-    stemmer = PorterStemmer()
-    return stemmer.stem_sentence(text)
-
-##############################
-def clean_text(text:str)->str:
+def clean_text(text:str, verbose:bool=False)->str:
     # Remove URLs
     t = time.time()
     out_text = remove_urls(text)
@@ -259,8 +221,43 @@ def clean_text(text:str)->str:
     t = time.time()
     out_text = remove_whitespaces(out_text)
     d8 = time.time() - t
-    print(f"URLs:\t{d1}\n", f"Emoticons:\t{d2}\n", f"Emojis:\t{d10}\n",
-          f"Neutral Emojis:\t{d11}\n", f"Slang:\t{d3}\n", f"Lowercase:\t{d4}\n",
-          f"Digits:\t{d5}\n", f"Stopwords:\t{d6}\n", f"Punctuations:\t{d7}\n",
-          f"Whitespaces:\t{d8}\n")
+
+    if verbose:
+        print(f"URLs:\t{d1}\n", f"Emoticons:\t{d2}\n", f"Emojis:\t{d10}\n",
+            f"Neutral Emojis:\t{d11}\n", f"Slang:\t{d3}\n",
+            f"Lowercase:\t{d4}\n", f"Decontractions:\t{d9}", f"Digits:\t{d5}\n",
+            f"Stopwords:\t{d6}\n", f"Punctuations:\t{d7}\n",
+            f"Whitespaces:\t{d8}\n")
+
     return out_text
+
+##############################
+# 
+##############################
+def lemmatization(text:str)->str:
+    lemmatizer = WordNetLemmatizer()
+    return " ".join([lemmatizer.lemmatize(word) for word in text.split()])
+
+def stemming(text:str)->str:
+    stemmer = PorterStemmer()
+    return stemmer.stem_sentence(text)
+
+def extract_hashtag(text:str)->List[str]:
+    """It extracts all the hashtags from the text and it collects all of them in
+    a list.
+
+    Parameters
+    ----------
+    text : str
+        Text to preprocess.
+
+    Returns
+    -------
+    List[str]
+        List of hashtag without the '#'
+    """
+    hashtag = re.findall(r"#(\w+)", text)
+    if hashtag:
+        #check if list is not empty
+        hashtag = [w.lower() for w in hashtag] #lower case
+    return hashtag
