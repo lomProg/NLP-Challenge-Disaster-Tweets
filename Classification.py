8daad76687738fc507896178d5c1f49c7a6a8d10
lorenzo-mora <l.mora4@campus.unimib.it>
2023-05-15 23:59:08 +0200

    Class definition for generating data
    Func split_data for train-test splitting and func tokenize_data for tokenization of various sets.

--- Classification.py
+++ Classification.py
@@ -1,4 +1,70 @@
+import re
+import pandas as pd
 from sklearn.model_selection import train_test_split
+from keras.preprocessing.text import Tokenizer
+from tensorflow.keras.utils import pad_sequences
+
+class DataGenerator(object):
+
+
+    def __init__(self) -> None:
+        pass
+
+    def split_data(
+            self, x:pd.Series, y:pd.Series,
+            reset_index:bool=False, **kwargs) -> None:
+
+        data = {}
+
+        train_sz = None
+        if any(re.search("test_size", k) for k in kwargs.keys()):
+            test_sz = kwargs["test_size"]
+        elif any(re.search("train_size", k) for k in kwargs.keys()):
+            train_sz = kwargs["train_size"]
+            test_sz = 1.0 - train_sz
+        else:
+            test_sz = 0.3
+        rnd_st = kwargs.get("random_state", None)
+
+        # Splitting input data
+        (X_train, X_test,
+         y_train, y_test) = train_test_split(x, y,
+                                             test_size = test_sz,
+                                             train_size = train_sz,
+                                             random_state = rnd_st,
+                                             **kwargs)
+        if reset_index:
+            X_train = X_train.reset_index()
+            X_test = X_test.reset_index()
+            y_train = y_train.reset_index()
+            y_test = y_test.reset_index()
+
+        data['x_train'] = X_train
+        data['x_test'] = X_test
+        data['y_train'] = y_train
+        data['y_test'] = y_test
+        self.data = data
+    
+    def tokenize_data(self, *args, max_sequence_length:int=50) -> None:
+
+        if not hasattr(self, "data"):
+            data = {}
+        else:
+            data = self.data
+
+        # Data tokenization
+        tokenizer = Tokenizer()
+        tokenizer.fit_on_texts(pd.concat([*args]))
+
+        # Vectorizing data to keep `max_sequence_length` words per sample
+        for i in range(len(args)):
+            data[f'vect_set_{i}'] = pad_sequences(
+                tokenizer.texts_to_sequences(args[i]),
+                maxlen = max_sequence_length,
+                padding = "post",
+                truncating = "post",
+                value = 0.)
+        self.data = data
 
 def split_train_test(data, test_size = 0.3, shuffle = True):
     X_train, X_test, Y_train, Y_test = train_test_split(data[['cleaned_text',
